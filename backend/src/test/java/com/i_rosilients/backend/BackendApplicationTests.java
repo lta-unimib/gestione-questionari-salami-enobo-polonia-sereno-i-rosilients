c66bb4505c4dbf9bafdc302af0fcb24bb1bc7497
Polonia Luca <l.polonia@campus.unimib.it>
2025-01-31 18:00:16 +0100

    inizializzazione

---
+++ backend/src/test/java/com/i_rosilients/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i_rosilients.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
