a76b32d18d619fa42468dde6dc73e179b72043a4
fenobobelomo <f.enobobelomo@campus.unimib.it>
2025-02-06 16:30:27 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,10 @@ import React  from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home/Home";
+import HomeLogged from "./pages/home/HomeLogged";
 
-import Navbar from './components/NavbarHome'
+import Navbar from './components/NavbarHome';
+import NavBarLogged from './components/NavbarLogged';
 
 const App = () => {
 
